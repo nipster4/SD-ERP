7f4aa2182dd7aff494ab5bc636656d8412ed4416
koonrkritt <koonkritt@yahoo.co.th>
2017-04-19 19:09:53 +0700

    หน้าเว็บ
    ยังไม่ได้ลิงค์ไปหาอันไหนน่ะ

---
+++ WearToWhere/index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <title>Wear To Where</title>
+    <meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+      
+      <link href="css/bootstrap.css" rel="stylesheet">
+      <link href="css/style.css" rel="stylesheet">
+      <link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
+      <link href="fonts/font.css" rel="stylesheet">
+
+  </head>
+  <body>
+  
+  	<div class="container">
+	<nav class="navbar navbar-custom">
+	  <div class="container-fluid">
+		<div class="navbar-header">
+		  <a class="navbar-brand" href="#">Wear to Where</a>
+		</div>
+		<ul class="nav navbar-nav">
+
+		  <li><a href="#"><i class="fa fa-shopping-cart" aria-hidden="true" style="font-size: 15px"></i> ซื้อ</a></li>
+		  <li><a href="#"><i class="fa fa-money" aria-hidden="true" style="font-size: 15px"></i> ขาย</a></li>
+		  <li><a href="#"><i class="fa fa-shopping-basket" aria-hidden="true" style="font-size: 15px"></i> สินค้า</a></li>
+		  <li><a href="#"><i class="fa fa-user-circle-o" aria-hidden="true" style="font-size: 15px"></i> สมุดรายชื่อ</a></li>
+		  <li><a href="#"><i class="fa fa-file-text" aria-hidden="true" style="font-size: 15px"></i> รายงาน</a></li>
+		</ul>
+		<ul class="nav navbar-nav navbar-right">
+		  <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+		  <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+		</ul>
+	  </div>
+	</nav>
+	</div>
+	
+	<div class="container">
+		<div class="menu-dashboard">
+			<h1 class="text-center">วันนี้ทำอะไรดี ?</h1>
+			<hr style="color:#2599cd;width:18%;border-width: 2px 0 0;border-color:#2599cd;margin-bottom:10px;margin-top:10px;">
+			<div class="container-fluid container-width" style="padding-top: 15px;">
+
+			<div class="col-md-4 text-center border-custom1">
+                            <a href="javascript:createUpdateDoc('Quotation')" class="ahover">
+                                <div class="menu-img-circle">
+                                    <span class="box-icon">
+                                        <i class="fa fa-file-word-o" style="font-size:80px;"></i>
+                                    </span>
+                                </div>
+                                <div class="menu-text-circle">
+                                   	<i class="glyphicon glyphicon-plus grid-btn" style="top:0;"></i>
+									<span class="qtmenutext"><b>Inquiry</b></span>
+                                </div>
+                            </a>
+							
+                            <div class="btn-container">
+                                <button onclick="javascript: window.location.hash = '#/Quotation'" type="button" class="btn btn-custom btn-list">ดูรายการทั้งหมด</button>
+                            </div>
+            </div>
+            
+            <div class="col-md-4 text-center border-custom1">
+                            <a href="javascript:createUpdateDoc('Quotation')" class="ahover">
+                                <div class="menu-img-circle">
+                                    <span class="box-icon">
+                                        <i class="fa fa-file-text-o" style="font-size:80px"></i>
+                                    </span>
+                                </div>
+                                <div class="menu-text-circle">
+                                   <i class="glyphicon glyphicon-plus grid-btn" style="top:0;"></i>
+									<span class="qtmenutext"><b>Quatation</b></span>
+                                </div>
+                            </a>
+
+                            <div class="btn-container">
+                                <button onclick="javascript: window.location.hash = '#/Quotation'" type="button" class="btn btn-custom btn-list">ดูรายการทั้งหมด</button>
+                            </div>
+            </div>	
+            <div class="col-md-4 text-center border-custom1">
+                            <a href="javascript:createUpdateDoc('Quotation')" class="ahover">
+                                <div class="menu-img-circle">
+                                    <span class="box-icon">
+                                        <i class="fa fa-file-powerpoint-o" style="font-size: 80px;"></i>
+                                    </span>
+                                </div>
+                                <div class="menu-text-circle">
+                                   <i class="glyphicon glyphicon-plus grid-btn" style="top:0;"></i>
+									<span class="qtmenutext"><b>PO</b></span>
+                                </div>
+                            </a>
+
+                            <div class="btn-container">
+                                <button onclick="javascript: window.location.hash = '#/Quotation'" type="button" class="btn btn-custom btn-list">ดูรายการทั้งหมด</button>
+                            </div>
+            </div>
+			</div>
+			
+			
+			<div class="container-fluid container-width" style="padding-top: 15px;">
+
+			<div class="col-md-4 text-center border-custom2">
+                            <a href="javascript:createUpdateDoc('Quotation')" class="ahover">
+                                <div class="menu-img-circle">
+                                    <span class="box-icon">
+                                        <i class="fa fa-book" style="font-size:80px;"></i>
+                                    </span>
+                                </div>
+                                <div class="menu-text-circle">
+                                   <i class="glyphicon glyphicon-plus grid-btn" style="top:0;"></i>
+									<span class="qtmenutext"><b>Sale Order</b></span>
+                                </div>
+                            </a>
+
+                            <div class="btn-container">
+                                <button onclick="javascript: window.location.hash = '#/Quotation'" type="button" class="btn btn-custom btn-list">ดูรายการทั้งหมด</button>
+                            </div>
+            </div>
+            
+            <div class="col-md-4 text-center border-custom2">
+                            <a href="javascript:createUpdateDoc('Quotation')" class="ahover">
+                                <div class="menu-img-circle">
+                                    <span class="box-icon">
+                                        <i class="fa fa-archive" style="font-size:80px"></i>
+                                    </span>
+                                </div>
+                                <div class="menu-text-circle">
+                                   <i class="glyphicon glyphicon-plus grid-btn" style="top:0;"></i>
+									<span class="qtmenutext"><b>Add Product</b></span>
+                                </div>
+                            </a>
+
+                            <div class="btn-container">
+                                <button onclick="javascript: window.location.hash = '#/Quotation'" type="button" class="btn btn-custom btn-list">ดูรายการทั้งหมด</button>
+                            </div>
+            </div>	
+            <div class="col-md-4 text-center border-custom2">
+                            <a href="javascript:createUpdateDoc('Quotation')" class="ahover">
+                                <div class="menu-img-circle">
+                                    <span class="box-icon">
+                                        <i class="fa fa-user-plus" style="font-size: 80px"></i>
+                                    </span>
+                                </div>
+                                <div class="menu-text-circle">
+                                    <i class="glyphicon glyphicon-plus grid-btn" style="top:0;"></i>
+									<span class="qtmenutext"><b>Add Customer</b></span>
+                                </div>
+                            </a>
+
+                            <div class="btn-container">
+                                <button onclick="javascript: window.location.hash = '#/Quotation'" type="button" class="btn btn-custom btn-list">ดูรายการทั้งหมด</button>
+                            </div>
+            </div>
+			</div>
+			
+			
+		</div>
+		
+	</div>
+	<br>
+	<div class="container">
+		<div class="container-footer">
+			
+		</div>
+	</div>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+  	<script type="text/javascript" src="js/bootstrap.js"></script>
+  </body>
+</html>
